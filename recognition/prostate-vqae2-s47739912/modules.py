ec569f4e08d6327c4b838ded834d8477ef4775e9
James Miller <jameswil.miller@gmail.com>
2025-10-09 03:39:31 +1000

    got training working without validation managed to finish 10 epochs with loss 25000,  reconstructions are somewhat clear

--- recognition/prostate-vqae2-s47739912/modules.py
+++ recognition/prostate-vqae2-s47739912/modules.py
@@ -7,7 +7,7 @@ class Parameters():
         self.batch_size = 32
         self.learning_rate = 1e-4
         self.embedding_dimension = 64
-
+        
 
 class CNNVAE(nn.Module):
     def __init__(self, latent_dim=64):
@@ -44,7 +44,7 @@ class CNNVAE(nn.Module):
         )
         #latent space params
         self.fc_mu = nn.Linear(256 * 8 * 8, latent_dim) #mean
-        self.log_var = nn.Linear(256 * 8 * 8, latent_dim) #log variance
+        self.fc_logvar = nn.Linear(256 * 8 * 8, latent_dim) #log variance
 
         #decoder
         self.decoder = nn.Sequential(
@@ -58,7 +58,7 @@ class CNNVAE(nn.Module):
 
             nn.ConvTranspose2d(256, 128, 3, stride=2, padding=1, output_padding=1), #16 > 32
             nn.BatchNorm2d(128),
-            nn.RelU(),
+            nn.ReLU(),
 
             nn.ConvTranspose2d(128, 64, 3, stride=2, padding=1, output_padding=1), #32 > 64
             nn.BatchNorm2d(64),
@@ -69,7 +69,6 @@ class CNNVAE(nn.Module):
             nn.ReLU(),
 
             nn.ConvTranspose2d(32, 1, 3, stride=2, padding=1, output_padding=1), #128 > 256
-            nn.BatchNorm2d(32),
             nn.Sigmoid(), #output in [0, 1] for img reconstruction
         )
         
@@ -95,10 +94,10 @@ class CNNVAE(nn.Module):
         recon = self.decode(z)
         return recon, mu, logvar
     
-    def vae_loss_function(recon_x, x, mu, logvar, beta=1.0):
-        #reconstruction loss (binary cross entropy)
-        BCE = nn.functional.binary_cross_entropy(recon_x, x, reduction='sum')
+def vae_loss_function(recon_x, x, mu, logvar, beta=1.0):
+    #reconstruction loss (binary cross entropy)
+    BCE = nn.functional.binary_cross_entropy(recon_x, x, reduction='sum')
 
-        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-        return BCE + beta * KLD, BCE, KLD
+    return BCE + beta * KLD, BCE, KLD
--- recognition/prostate-vqae2-s47739912/train.py
+++ recognition/prostate-vqae2-s47739912/train.py
@@ -2,8 +2,9 @@ import torch
 from modules import *
 from dataset import *
 
+import matplotlib.pyplot as plt
 
-device = torch.device('cuda' if torch.cuda_is_available() else 'cpu')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if not torch.cuda.is_available():
     print("Warning CUDA not found. Using CPU")
 
@@ -23,4 +24,32 @@ beta = 1.0
 
 print("training VAE...")
 
+
+for epoch in range(epochs):
+    vae.train()
+    total_loss = 0.0
+    total_bce = 0.0
+    total_kld = 0.0
+
+    for batch_idx, images in enumerate(train_loader):
+        images = images.to(device)
+
+        #forward
+        recon_images, mu, logvar = vae(images)
+
+        #loss
+        loss, bce, kld = vae_loss_function(recon_images, images, mu, logvar, 0.5)
+
+        #backward pass
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        total_loss += loss.item()
+        total_bce += bce.item()
+        total_kld += kld.item()
+        
+    avg_loss =total_loss / len(train_loader.dataset)
+    avg_bce = total_bce / len(train_loader.dataset)
+    avg_kld = total_kld / len(train_loader.dataset)
 
